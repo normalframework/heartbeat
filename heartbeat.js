57642ea233caaca8c29e8f13474c6a8403d96c16
Stephen Dawson-Haggerty <sdhags@gmail.com>
2025-05-16 14:19:35 -0400

    Create BACnet Object

--- heartbeat.js
+++ heartbeat.js
@@ -7,7 +7,29 @@ const NormalSdk = require("@normalframework/applications-sdk");
  */
 module.exports = async ({globalVariables, sdk, update, args}) => {
   let heartbeat = globalVariables[0] 
+
+  try { 
+    await sdk.http.post("/api/v1/bacnet/local", {
+      "uuid": heartbeat.uuid,
+      "object_id": {
+        "object_type": "OBJECT_BINARY_VALUE"
+      },
+      "props": [ 
+        {
+          "property": "PROP_OBJECT_NAME",
+          "value" : {
+            "character_string": "Heartbeat"
+          }
+        }
+      ] 
+    })
+  } catch (e) {
+     if (e.response?.status !== 409) {
+       console.error("BACnet POST error:", e.response?.data || e.message || e);
+     }
+  }
+    
   let newval = heartbeat.latestValue.value  ? 0 : 1;
   sdk.logEvent(`setting heartbeat to ${newval}`)
-  heartbeat.write(newval)
+  await heartbeat.write(newval)
 };
